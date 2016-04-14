3e8479328a31969c430f3b6d1132ab7a702b6125
Christopher Yang <cyang001@citymail.cuny.edu>
2016-04-13 21:05:04 -0400

    Rename functions

--- src/Parser.java
+++ src/Parser.java
@@ -15,7 +15,7 @@ public final class Parser {
     private static Stack<Token> tokenStack;
 
     private static ArrayList<Byte> p_code = new ArrayList<>();
-    private static Byte ip = 0;
+    private static int ip = 0;
 
     public static void parse() {
         getToken(); // Get initial token
@@ -50,7 +50,7 @@ public final class Parser {
 	-> E
      */
     public static void declarations() {
-        var_declarations();
+        varDeclarations();
         // TODO
     }
 
@@ -59,7 +59,7 @@ public final class Parser {
     <var decl> ->
         var[<namelist>: <type>;]^+
      */
-    public static void var_declarations() {
+    public static void varDeclarations() {
         while(true) {
             if ("TK_VAR".equals(currentToken.getTokenType())) {
                 match("TK_VAR");
@@ -122,18 +122,38 @@ public final class Parser {
 	    <assignment> TK_A_VAR
 	    <labelling> TK_A_LABEL
 	    <procedure call> TK_A_PROC
-	    <write_stat>
+	    <writeStat>
      */
     public static void statements(){
         // TODO finish statements
         switch (currentToken.getTokenType()){
             case "TK_WRITELN":
-                write_stat();
+                writeStat();
                 break;
         }
     }
 
-    public static void write_stat(){
+    public static void ifStat(){
+        match("TK_IF");
+        cond();
+        match("TK_THEN");
+        gen_opcode(OP_JFALSE);
+        int hole = ip; genAddr(0);
+        statements();
+
+        if(currentToken.getTokenType().equals("TK_ELSE")) {
+            genOpcode("OP_JMP");
+            int hole2 = ip; genAddr(0);
+            save = ip; ip = hole; genAddr(save); ip = save;
+            hole = hole2; statements(); match("TK_ELSE"); statements();
+        }
+
+        int save = ip; ip = hole; genAddr(save); ip = save;
+
+    }
+
+
+    public static void writeStat(){
         match("TK_WRITELN");
         match("TK_OPEN_PARENTHESIS");
 
@@ -186,7 +206,7 @@ public final class Parser {
             error();
     }
 
-    public static void assignment_stat() {
+    public static void assignmentStat() {
         //save type, address into LHS_type, LHS_addr
         getToken();
         match("TK_ASSIGNMENT");
@@ -210,12 +230,12 @@ public final class Parser {
 
     }
 
-    public static void gen_address(int a){
+    public static void genAddress(int a){
         *(int*)(code+ip) = a;
         ip+=sizeof(int);
     }
 
-    public static void gen_opcode(char b){
+    public static void genOpcode(char b){
         p_code.get(ip++)=b;
     }
 
@@ -227,7 +247,7 @@ public final class Parser {
 
     public static void match(String tokenType) {
         if (!tokenType.equals(currentToken.getTokenType())) {
-            throw new Error("The token does not match the current token");
+            throw new Error(String.format("The token: %s does not match the current token: %s", tokenType, currentToken.getTokenType()));
         } else {
             System.out.println(String.format("matched: %s", currentToken.getTokenType()));
             getToken();
--- src/Token.java
+++ src/Token.java
@@ -18,10 +18,6 @@ public final class Token {
         return tokenValue;
     }
 
-//    public boolean equals(Token rhs) {
-//        return (this.getTokenType().equals(rhs.getTokenType()) && (this.getTokenValue().equals(rhs.getTokenValue())));
-//    }
-
     public String getTokenType() {
         return tokenType;
     }
